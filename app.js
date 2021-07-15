869ab58a90f46f51296494f578f07f7414049437
Tarun <tarundyundi2000@gmail.com>
2021-07-16 02:15:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.post('/',function(req,res){
   res.redirect("/")
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
